d8b5db3e64bb265a6d5d9ca69857e1515d2c7c1f
Ulises Fasoli <ufasoli@gmail.com>
2014-02-07 09:46:47 +0100

    Code cleaning

--- spring-boot-hello-world-ws/src/main/java/com/ufasoli/tutorials/spring/boot/web/ui/SpringVaadinUI.java
+++ spring-boot-hello-world-ws/src/main/java/com/ufasoli/tutorials/spring/boot/web/ui/SpringVaadinUI.java
@@ -1,15 +1,8 @@
 package com.ufasoli.tutorials.spring.boot.web.ui;
 
-import com.google.gwt.thirdparty.guava.common.collect.Lists;
-import com.ufasoli.tutorials.spring.boot.model.Book;
-import com.ufasoli.tutorials.spring.boot.repositories.BookRepository;
-import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.MenuBar;
-import com.vaadin.ui.Table;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.vaadin.spring.VaadinUI;
 import org.vaadin.spring.navigator.SpringViewProvider;
@@ -30,17 +23,11 @@ public class SpringVaadinUI extends UI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
 
+        getPage().setTitle("Spring boot Vaadin UI");
 
-
-        getPage().setTitle("Vaadin UI");
-
-
-        Navigator navigator = new Navigator(this,this);
+        Navigator navigator = new Navigator(this, this);
         navigator.addProvider(viewProvider);
 
         setNavigator(navigator);
-
-
-
     }
 }
