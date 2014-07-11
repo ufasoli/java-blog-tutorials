9a20d1a3f3459c7971f8d12e1fe6afb4aa56881d
Ulises Fasoli <ufasoli@gmail.com>
2014-07-11 13:41:47 +0200

    added new tutorial

---
+++ maven-custom-assembly/src/main/java/com/ufasoli/maven/custom/assembly/Main.java
@@ -0,0 +1,14 @@
+package com.ufasoli.maven.custom.assembly;
+
+/**
+ * User: Ulises Fasoli
+ * Date: 23.06.2014
+ * Time: 18:03
+ * Project : maven-custom-assembly
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD !");
+    }
+}
